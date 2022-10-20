27170067e2e0a322cf56cad7d012fd5a109e250a
UGeunJi <84713532+UGeunJi@users.noreply.github.com>
2022-10-20 14:03:36 +0900

    Create 중앙값 구하기.py

---
+++ coding test Lv.0/중앙값 구하기.py
@@ -0,0 +1,7 @@
+중앙값은 어떤 주어진 값들을 크기의 순서대로 정렬했을 때 가장 중앙에 위치하는 값을 의미합니다. 예를 들어 1, 2, 7, 10, 11의 중앙값은 7입니다. 정수 배열 array가 매개변수로 주어질 때, 중앙값을 return 하도록 solution 함수를 완성해보세요.
+
+def solution(array):
+    answer = 0
+    array.sort()
+    answer = array[int(len(array) / 2)]
+    return answer
