d885979e84039625e1b8413a83f67d4b728144ed
UGeunJi <84713532+UGeunJi@users.noreply.github.com>
2022-10-25 17:07:05 +0900

    Create 최댓값 만들기.py

---
+++ coding test Lv.0/최댓값 만들기.py
@@ -0,0 +1,7 @@
+정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
+
+def solution(numbers):
+    answer = 0
+    numbers.sort(reverse = True)
+    answer = numbers[0] * numbers[1]
+    return answer
