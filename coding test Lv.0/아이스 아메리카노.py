8b1b33637280d0fa9df47d9e4e53bba1becfeddb
UGeunJi <84713532+UGeunJi@users.noreply.github.com>
2022-10-25 12:44:29 +0900

    Create 아이스 아메리카노.py

---
+++ coding test Lv.0/아이스 아메리카노.py
@@ -0,0 +1,12 @@
+머쓱이는 추운 날에도 아이스 아메리카노만 마십니다. 아이스 아메리카노는 한잔에 5,500원입니다. 머쓱이가 가지고 있는 돈 money가 매개변수로 주어질 때, 머쓱이가 최대로 마실 수 있는 아메리카노의 잔 수와 남는 돈을 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+
+def solution(money):
+    answer = []
+    a = 0
+    while money >= 5499:
+        money -= 5500
+        a += 1
+        
+    answer.append(a)
+    answer.append(money)    
+    return answer
