00411a8c20d86ed6725edf18d167a725426607bf
UGeunJi <84713532+UGeunJi@users.noreply.github.com>
2022-10-25 11:56:41 +0900

    Create 배열 뒤집기.py

---
+++ coding test Lv.0/배열 뒤집기.py
@@ -0,0 +1,6 @@
+정수가 들어 있는 배열 num_list가 매개변수로 주어집니다. num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
+
+def solution(num_list):
+    num_list.reverse()
+    answer = num_list
+    return answer
