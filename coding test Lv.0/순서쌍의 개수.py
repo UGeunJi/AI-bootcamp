9adc7ac009a842e9a8c42bcadc7dcd732e4973bf
UGeunJi <84713532+UGeunJi@users.noreply.github.com>
2022-10-27 00:42:50 +0900

    Create 순서쌍의 개수.py

---
+++ coding test Lv.0/순서쌍의 개수.py
@@ -0,0 +1,9 @@
+순서쌍이란 두 개의 숫자를 순서를 정하여 짝지어 나타낸 쌍으로 (a, b)로 표기합니다.
+자연수 n이 매개변수로 주어질 때 두 숫자의 곱이 n인 자연수 순서쌍의 개수를 return하도록 solution 함수를 완성해주세요.
+
+def solution(n):
+    answer = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            answer += 1
+    return answer
