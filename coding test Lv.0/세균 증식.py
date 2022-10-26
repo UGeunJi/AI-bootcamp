d239d122d82bcf04b9fbccbdea93d90d03172334
UGeunJi <84713532+UGeunJi@users.noreply.github.com>
2022-10-26 17:43:43 +0900

    Create 세균 증식.py

---
+++ coding test Lv.0/세균 증식.py
@@ -0,0 +1,6 @@
+어떤 세균은 1시간에 두배만큼 증식한다고 합니다. 처음 세균의 마리수 n과 경과한 시간 t가 매개변수로 주어질 때 t시간 후 세균의 수를 return하도록 solution 함수를 완성해주세요.
+
+def solution(n, t):
+    answer = 0
+    answer = n * (2 ** t)
+    return answer
