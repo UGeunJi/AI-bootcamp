27b7f46571b12d5a0d84ae3056cf6306bab32325
UGeunJi <84713532+UGeunJi@users.noreply.github.com>
2022-11-16 14:33:17 +0900

    Update 2차원으로 만들기.py

--- coding test Lv.0/2차원으로 만들기.py
+++ coding test Lv.0/2차원으로 만들기.py
@@ -3,7 +3,7 @@
 num_list가 [1, 2, 3, 4, 5, 6, 7, 8] 로 길이가 8이고 n이 2이므로 num_list를 2 * 4 배열로 다음과 같이 변경합니다.
 2차원으로 바꿀 때에는 num_list의 원소들을 앞에서부터 n개씩 나눠 2차원 배열로 변경합니다.
 
-num_list	                n	                result
+ num_list	                n    result
 [1, 2, 3, 4, 5, 6, 7, 8]	2	[[1, 2], [3, 4], [5, 6], [7, 8]]
 
 def solution(num_list, n):
