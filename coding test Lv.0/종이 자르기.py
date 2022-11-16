9afcfad9304d813dec1cfc1fa7a6cbf180191251
UGeunJi <84713532+UGeunJi@users.noreply.github.com>
2022-11-16 16:01:17 +0900

    Create 종이 자르기.py

---
+++ coding test Lv.0/종이 자르기.py
@@ -0,0 +1,16 @@
+머쓱이는 큰 종이를 1 x 1 크기로 자르려고 합니다. 예를 들어 2 x 2 크기의 종이를 1 x 1 크기로 자르려면 최소 가위질 세 번이 필요합니다.
+
+정수 M, N이 매개변수로 주어질 때, M x N 크기의 종이를 최소로 가위질 해야하는 횟수를 return 하도록 solution 함수를 완성해보세요.
+
+M	 N	result
+2	 2	3
+2	 5	9
+1	 1	0
+
+
+
+
+def solution(M, N):
+    answer = 0
+    answer = M * N - 1
+    return answer
