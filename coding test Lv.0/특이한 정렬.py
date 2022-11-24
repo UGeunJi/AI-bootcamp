45567ea8c4b6aa34071c3c54cb944954be0f8003
UGeunJi <84713532+UGeunJi@users.noreply.github.com>
2022-11-24 11:54:59 +0900

    Create 특이한 정렬.py

---
+++ coding test Lv.0/특이한 정렬.py
@@ -0,0 +1,8 @@
+정수 n을 기준으로 n과 가까운 수부터 정렬하려고 합니다. 이때 n으로부터의 거리가 같다면 더 큰 수를 앞에 오도록 배치합니다.
+정수가 담긴 배열 numlist와 정수 n이 주어질 때 numlist의 원소를 n으로부터 가까운 순서대로 정렬한 배열을 return하도록 solution 함수를 완성해주세요.
+
+def solution(numlist, n):
+    
+    answer = sorted(numlist, key = lambda x : (abs(n - x), -x))
+        
+    return answer
